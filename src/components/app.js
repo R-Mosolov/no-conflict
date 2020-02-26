8c2c674a0f8185d7fa7a09283eebbbc9cd284263
R-Mosolov <r.v.mosolov@ya.ru>
2020-02-26 20:48:10 +0300

    #5@1h29m: create template of React project, add .gitignore

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./logo";
+
+function App() {
+  return (
+    <div className="App container">
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
