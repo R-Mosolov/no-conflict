aaec4f7eb9ada643c6d281444d1272c8bf1b6a87
R-Mosolov <r.v.mosolov@ya.ru>
2020-02-27 06:26:40 +0300

    #8@1h34m: add universal function to generate questions

--- src/components/conflict-type.jsx
+++ src/components/conflict-type.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 
 function ConflictType() {
-    function createAnswer(id, labelName, radioName) {
-        return (
-            <div className="input-group mt-1">
-                <div className="input-group-prepend">
-                    <div className="input-group-text"><input type="radio" name={ radioName } id={ id } /></div>
-                </div>
+    function createQuestionAndAnswers(radioName, question, idsAndLabelNames) {
+        function createAnswers() {
+            let arr = [];
+            const iterable = new Map(idsAndLabelNames);
 
-                <label className="form-control" htmlFor={ id }>{labelName}</label>
+            for (let [id, labelName] of iterable) {
+                arr.push(
+                    <div className="input-group mt-1">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text"><input type="radio" name={ radioName } id={ id }/></div>
+                        </div>
+
+                        <label className="form-control" htmlFor={ id }>{ labelName }</label>
+                    </div>
+                );
+            }
+
+            return arr;
+        }
+
+        return (
+            <div>
+                <p className="mt-4">{ question }</p>
+
+                { createAnswers() }
             </div>
         )
     }
@@ -17,10 +34,10 @@ function ConflictType() {
         <div>
             <h1>1. Тип конфликта</h1>
 
-            <p className="mt-4">1.1. Выберите тип Вашего конфликта</p>
-            { createAnswer('psychological', 'Психологический', 'conflict-type') }
-            { createAnswer('psycho', 'Психический', 'conflict-type') }
-            { createAnswer('social', 'Социальный', 'conflict-type') }
+            { createQuestionAndAnswers(
+                'conflict-type','1.1. Выберите тип Вашего конфликта',
+                [['psychological', 'Психологический'], ['psycho', 'Психический'], ['social', 'Социальный']]
+            ) }
 
             <button className="mt-3 btn btn-success">Далее</button>
         </div>
