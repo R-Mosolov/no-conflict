221b732cf9cc490d0635d65feb9d9b9a76c041e9
R-Mosolov <r.v.mosolov@ya.ru>
2020-02-29 20:24:31 +0300

    #12@0h08m: add Procfile to deploy app on Heroku

--- src/directives/choose-answer.jsx
+++ src/directives/choose-answer.jsx
@@ -2,7 +2,7 @@ function chooseAnswer() {
     const navigation = document.getElementById('navigation');
 
     return navigation.insertAdjacentHTML('beforebegin',
-        '<div style="margin-top: 12px"><b>Пожалуйста, выберите один из вариантов ответа.</b></div>');
+        '<div style="margin-top: 12px; color: red"><b>Пожалуйста, выберите один из вариантов ответа.</b></div>');
 }
 
 export default chooseAnswer;
