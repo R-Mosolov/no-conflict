636488d139ce2a0d226e8743a4faf651a0f4889d
R-Mosolov <r.v.mosolov@ya.ru>
2020-03-25 16:45:05 +0300

    #47@0h20m: delete api and server dirs to deploy

--- api/routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var ctrlAnswers = require('../controllers/answers');
-
-// LOCATIONS ROUTERS
-router.get('/answers', ctrlAnswers.answersRead);
-
-module.exports = router;
